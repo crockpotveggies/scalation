ec32a2dd6cedf1a63b76a0c7b0c09176dab0721d
mepcotterell@gmail.com <mepcotterell@gmail.com@27687dc2-ac71-eaae-a691-6d7cc31676af>
2011-02-23 21:56:36 +0000

    Added an example application of how to take advantage of a RichAny

--- examples/rich/RichSet.scala
+++ examples/rich/RichSet.scala
@@ -8,7 +8,41 @@ import scalation.ScalaTion
  */
 object RichSet extends ScalaTion
 {
-	def main(args : Array[String]) : Unit = {
-		// coming soon
+	def printSection(title: String)
+	{
+		println
+		for (i <- 1 to 80) print(":"); println
+		println(title.toUpperCase)
+		for (i <- 1 to 80) print(":"); println
+		println
+	}
+	
+	def main(args : Array[String]) 
+	{
+		
+		println("Example of how to take advantage of RichSet.")
+		
+		printSection("Define sets")
+		
+		val ab = Set("a", "b")
+		val ac = Set("a", "c")
+		val bc = Set("b", "c")
+		
+		println("ab = " + ab)
+		println("ac = " + ac)
+		println("bc = " + bc)
+		
+		printSection("Contains")
+		
+		println("ab ∋ a = " + ab ∋ "a")
+		println("ab ∋ b = " + ab ∋ "b")
+		println("ab ∋ c = " + ab ∋ "c")
+		
+		println("ab ∌ a = " + ab ∌ "a")
+		println("ab ∌ b = " + ab ∌ "b")
+		println("ab ∌ c = " + ab ∌ "c")
+		
+		// more examples to come
+		
 	}
 }
