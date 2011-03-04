b28bf329e48771cb1fc0b44b21abeac4b9022e63
mepcotterell@gmail.com <mepcotterell@gmail.com@27687dc2-ac71-eaae-a691-6d7cc31676af>
2011-03-04 02:49:26 +0000

    Implemented multiple up-arrow Knuth notation.

--- src/scalation/rich/RichNumeric.scala
+++ src/scalation/rich/RichNumeric.scala
@@ -4,19 +4,23 @@ class RichNumeric[N: Numeric](elem: N) extends scalation.ScalaTion {
 
 	private val evidence = implicitly[Numeric[N]]
 	
+	/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 	/**
 	 * The Pi function for real numbers n no less than 0
 	 */
-	private def gaussPi(n: Double): Double = {
+	private def gaussPi(n: Double): Double = 
+	{
 		import scala.math._
 		// finds value to 2 decimal places
 		∏(1, 30000, (k: Double) => (((k + 1) / k) ↑ n) * (k / (n + k)))
     }
 	
+	/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 	/**
 	 * Returns the factorial
 	 */
-	def !(): N = {
+	def !(): N = 
+	{
 		if (elem == evidence.toInt(elem)) {
 			// treat elem as integral type
 			∏(1 to evidence.toInt(elem)).asInstanceOf[N]
@@ -26,15 +30,26 @@ class RichNumeric[N: Numeric](elem: N) extends scalation.ScalaTion {
 		}
 	}
 	
+	/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 	/**
-	 * Returns the value of the number raised to the p power.
+	 * Returns the value of the number raised to the p power as a Double. Using
+	 * Knuth's up-arrow notation
 	 */
-	def ↑[A: Numeric](p: A): N =
+	def ↑[A: Numeric](p: A): Double =
 	{
 		import scala.math._
 		val x = evidence.toDouble(elem)
 		val y = implicitly[Numeric[A]].toDouble(p)
-		pow(x, y).asInstanceOf[N]
+		pow(x, y)
+	}
+	
+	def ↑(n: Int, b: Int): Double = 
+	{
+		(n, b) match {
+			case (1, _) => elem ↑ b
+			case (_, 0) => 1
+			case (_, _)	=> (elem ↑ (n-1)) ↑ (elem ↑ (n, b-1))
+		}
 	}
 	
 }
