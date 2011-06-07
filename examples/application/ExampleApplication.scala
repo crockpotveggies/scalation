69dcdcfc0251cc4c0fbeb0859479c008f0e266fe
mepcotterell@gmail.com <mepcotterell@gmail.com@27687dc2-ac71-eaae-a691-6d7cc31676af>
2011-06-07 16:09:59 +0000

    Ensured that product series works. Included examples.

--- examples/application/ExampleApplication.scala
+++ examples/application/ExampleApplication.scala
@@ -1,5 +1,10 @@
 /* $Id$ */
 
+/**:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ * @author  John Miller, Michael Cotterell
+ * @see     LICENSE (MIT style license file).
+ */
+
 package application
 
 // This is all we need to import in order to take advantage of the ScalaTion's
@@ -26,6 +31,7 @@ object ExampleApplication extends ScalaTion
 		
 	    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 		/* Exponentiation */
+	    
 	    val exp1 = 2↑2		// 4
 	    val exp2 = 2↑2↑2	// 16
 	    
@@ -35,6 +41,7 @@ object ExampleApplication extends ScalaTion
 	    
 	    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 		/* Roots */
+	    
 	    val root1 = 4↓2				// 2
 	    val root2 = 4↓2↓2			// 1.41421...
 	    val test  = 4↑0.5 == 4↓2	// true
@@ -46,6 +53,7 @@ object ExampleApplication extends ScalaTion
 	    
 	    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 		/* Factorials */
+	    
 	    val fac1 = 4!	// 24
 	    val fac2 = 3.5!	// 11.63172...
 	    
@@ -62,5 +70,26 @@ object ExampleApplication extends ScalaTion
 	    section("Rising and Falling Factorials")
 	    println("4 ⇑ 4 = %s".format(rising))
 	    println("4 ⇓ 4 = %s".format(falling))
+	    
+	    /*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		/* Product Series */
+	    
+	    val prod1 = ∏(1 to 3) 						// 6
+        val prod2 = ∏(1 to 3, (i: Int) ⇒ i ↑ 2)		// 36
+	    
+        val vec = Vec(1, 2, 3, 4)
+        
+        val prod3 = ∏(vec)							// 24
+        val prod4 = ∏(0 to 2, (i: Int) ⇒ vec(i))	// 6
+        
+	    section("Product Series")
+	    println("∏(1 to 3)             = %s".format(prod1))
+	    println("∏(1 to 3, i ⇒ i ↑ 2)  = %s".format(prod2))
+	    println("vec                   = %s".format(vec))
+	    println("∏(vec)                = %s".format(prod3))
+	    println("∏(0 to 2, i ⇒ vec(i)) = %s".format(prod4))
+	    
+	    
+	    
 	}
 }
