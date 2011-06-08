e51ef7ea168cdf6667f9c1e092e68bae8e732bb7
mepcotterell@gmail.com <mepcotterell@gmail.com@27687dc2-ac71-eaae-a691-6d7cc31676af>
2011-06-08 00:23:36 +0000

    Added some documentation to RandVec.

--- src/scalation/stat/RandVec.scala
+++ src/scalation/stat/RandVec.scala
@@ -3,23 +3,28 @@ package stat
 
 import advmath._
 
-object rand {
-    val r = new Random(0)
-    def gen = r.gen
-}
-
+/** Companion object for RandVec class.
+ */
 object RandVec {
     def ofLength(length: Int) = new RandVec(length)
 }
 
+/** The RandVec class is a random numeric vector of a specific length. If values
+ *  and probabilities are provided, then the mean statistic takes into
+ *  consideration the probability distribution. If no probability distribution
+ *  is provided, then RandVec assumes a uniform distribution of probabilities.
+ */
 class RandVec (length: Int, var v: VecD = null, var p: VecD = null) extends Vec[Double] (Array.ofDim[Double](length)) {
 
+    // random distribution
+    private val r = new Random(0)
+  
     {
         // if no probabilities are given, assume uniform distribution of probabilities
         if (p == null) p = Vec.fromSeq(Array.fill(length)(1.0/length).toSeq)
      
         // if no array is given, then provide a random one
-        if (v == null) v = Vec.fill[Double](length)(rand.gen)
+        if (v == null) v = Vec.fill[Double](length)(r.gen)
             
         for (i <- range) this(i) = v(i)
     }
